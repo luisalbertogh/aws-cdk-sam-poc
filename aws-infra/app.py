73eda903aad0ca9da4ab9681b35577ab9e7f23e1
Luis Alberto Garcia Hernandez <luisalbertogh@hotmail.com>
2026-05-03 20:45:12 +0200

    Comment out role for secret.

--- aws-infra/app.py
+++ aws-infra/app.py
@@ -64,7 +64,7 @@ secrets_stack = SecretsStack(
     app,
     "CloudPocSecretsStack",
     secret_name=CHEF_UI_ECS_CONFIG.login_secret_name,
-    reader_role_arn=cluster_stack.execution_role.role_arn,
+    # reader_role_arn=cluster_stack.execution_role.role_arn,
     env=env,
     description="Cloud POC — Secrets Manager secrets for Chef UI",
 )
